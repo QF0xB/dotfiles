959d2bd43403b1b45a8a5f417674394959d8131e
Quirin Brändli <qbraendli@pm.me>
2025-02-18 00:42:14 +0100

    Added ssh_host_keys to persist

--- modules/nixos/system/ssh-server.nix
+++ modules/nixos/system/ssh-server.nix
@@ -20,5 +20,16 @@ in
       
       settings.PasswordAuthentication = cfg.password-auth;
     };
+
+    qnix.persist = {
+      root = {
+        files = [
+          "/etc/ssh/ssh_host_rsa_key"
+          "/etc/ssh/ssh_host_rsa_key.pub"
+          "/etc/ssh/ssh_host_ed25519_key"
+          "/etc/ssh/ssh_host_ed25519_key.pub"
+        ];
+      };
+    };
   };
 }
