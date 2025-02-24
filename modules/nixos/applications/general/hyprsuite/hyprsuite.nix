4d842ea71e7c527074865186a33de42b1d317e70
Quirin Brändli <qbraendli@pm.me>
2025-02-24 23:51:32 +0000

    Updated hyprsuite option.

--- modules/nixos/applications/general/hyprsuite/hyprsuite.nix
+++ modules/nixos/applications/general/hyprsuite/hyprsuite.nix
@@ -4,12 +4,17 @@
   ...
 }:
 
+let
+  cfg = config.hm.qnix.applications.general.hyprsuite;
+in
 {
   imports = [
-    ./hyprland.nix 
+    ./hyprland.nix
   ];
 
-  config.qnix.applications.general.hyprsuite = with lib; {
-    hyprland.enable = true;
+  config.qnix.applications.general = lib.mkIf cfg.enable {
+    hyprsuite = with lib; {
+      hyprland.enable = mkDefault true;
+    };
   };
 }
