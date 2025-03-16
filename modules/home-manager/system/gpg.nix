dafe4327ce547066fc00691f37551ffcaa75ddff
Quirin Brändli <qbraendli@pm.me>
2025-03-16 11:34:00 +0100

    Fix gpg again

--- modules/home-manager/system/gpg.nix
+++ modules/home-manager/system/gpg.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 
@@ -9,6 +10,13 @@
     home.file.".gnupg/scdaemon.conf".text = ''
       disable-ccid
       debug-level guru
+    '';
+
+    home.file.".gnupg/gpg-agent.conf".text = ''
+      default-cache-ttl 60
+      max-cache-ttl 120
+      pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
+      ttyname $GPG_TTY
     '';
 
     home.activation.fixGpgHome = lib.mkAfter ''
