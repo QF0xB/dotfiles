4b7d6e1e2c7a2b025aca095386329d0da5c6dd22
Quirin Brändli <qbraendli@pm.me>
2025-06-24 16:28:51 +0200

    Add second yubikey

--- modules/home-manager/hardware/yubikey/yubikey.nix
+++ modules/home-manager/hardware/yubikey/yubikey.nix
@@ -18,7 +18,10 @@ in
 
   config = {
     # write Yubico file
-    xdg.configFile."Yubico/u2f_keys".text =
-      "lcqbraendli:NVMp7RLgO9G+J8I+iC2mj0qum9xswAnnYQJU6btNuxcpVrPZFJr96Iwa2qBh2i+MnYc3o701kZLTPlkWd5ztyw==,N2cOgisG3QHs6DDees4w6nrZK2LOKZF6tZQkYGCJ6p52kQ5TG9hreCFBR68UliyOSg4FYinlRK57L0mtVejrbw==,es256,+presence";
+    xdg.configFile."Yubico/u2f_keys".text = ''
+      lcqbraendli:NVMp7RLgO9G+J8I+iC2mj0qum9xswAnnYQJU6btNuxcpVrPZFJr96Iwa2qBh2i+MnYc3o701kZLTPlkWd5ztyw==,N2cOgisG3QHs6DDees4w6nrZK2LOKZF6tZQkYGCJ6p52kQ5TG9hreCFBR68UliyOSg4FYinlRK57L0mtVejrbw==,es256,+presence
+      lcqbraendli:wkV7lkqW9E+93Ly7eocHUwHWzPrWLGilfRchTImd9/OOWcHM/RRZeXculwZAa81KkqcVPDn2whCYQyR9Z8erSA==,fFC5hZ08YvRKIvehEk65vV8B2bpAgOCsZIUw2+yS4jR4A++jaHB2AfISxGHd/05IJ15XeMsbgM8d8Jtd1pryHA==,es256,+presence
+    '';
+
   };
 }
